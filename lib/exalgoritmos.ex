18e8b57d19bd70450af59cb3fb52b8f524bbfa36
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2021-05-05 21:48:49 -0300

    Criando projeto

---
+++ lib/exalgoritmos.ex
@@ -0,0 +1,18 @@
+defmodule Exalgoritmos do
+  @moduledoc """
+  Documentation for `Exalgoritmos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exalgoritmos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exalgoritmos_test.exs
@@ -0,0 +1,8 @@
+defmodule ExalgoritmosTest do
+  use ExUnit.Case
+  doctest Exalgoritmos
+
+  test "greets the world" do
+    assert Exalgoritmos.hello() == :world
+  end
+end
